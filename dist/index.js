fa7a8053059a0b842be8c976abb0bbad7bd3bb85
Lukas Müller <lukas.mueller@virtual-identity.com>
2018-05-07 14:35:19 +0200

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+function animatedScroll(elementY, duration) {
+    const startingY = window.pageYOffset;
+    const diff = elementY - startingY;
+    let start;
+    window.requestAnimationFrame(function step(timestamp) {
+        if (!start)
+            start = timestamp;
+        const time = timestamp - start;
+        const percent = Math.min(time / duration, 1);
+        window.scrollTo(0, startingY + diff * percent);
+        if (time < duration) {
+            window.requestAnimationFrame(step);
+        }
+    });
+}
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+function animatedScroll(elementY: number, duration: number) {
+	const startingY = window.pageYOffset;
+	const diff = elementY - startingY;
+	let start: number;
+
+	window.requestAnimationFrame(function step(timestamp: number) {
+		if (!start) start = timestamp;
+		const time = timestamp - start;
+		const percent = Math.min(time / duration, 1);
+		window.scrollTo(0, startingY + diff * percent);
+		if (time < duration) {
+			window.requestAnimationFrame(step);
+		}
+	})
+}
